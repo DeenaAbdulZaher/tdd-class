abaec2f20d7fb761e5cdc07fd4bf7166a1c550bc
Iwein Fuld <iwein@fuld.net>
2013-11-17 12:47:44 +0100

    Cleans up unused code

--- src/test/java/tdd/NumberNames.java
+++ src/test/java/tdd/NumberNames.java
@@ -4,31 +4,16 @@
  */
 package tdd;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  *
  * @author Gamal Shaban OLC
  */
 class NumberNames {
 
-  private NumberNamesRepository numberNameRepository = new EnglishNumberNamesRepository();
+  private NumberNamesRepository numberNameRepository =
+          new EnglishNumberNamesRepository();
 
   public String read(Integer digit){
         return numberNameRepository.nameOf(digit);
-    }
-    
-    private final Map<Integer,String> oneDigitMap= new HashMap<Integer,String>();
-    
-    private String readOneDigit(Integer digit)throws Exception{
-        if(digit.toString().length()>1){
-            throw new Exception("Digits Length must be only one digit.");
-        }
-        oneDigitMap.put(1, "one");
-        oneDigitMap.put(2, "two");
-        
-        
-        return null;
     }
 }
